08ccc7443b6512720d369c2a584b28b7979be54a
Daniel Byrne <dbyrne978@gmail.com>
2022-06-21 15:36:57 -0400

    Add morgan middleware for better request logging

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const morgan = require('morgan')
 const express = require('express')
 const app = express()
 
@@ -39,16 +40,11 @@ let db = {
 }
 
 // middleware
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
 app.use(express.json())
-app.use(requestLogger)
+
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - ' +
+    ':response-time ms -- :body'))
 
 // routes
 app.get('/info', (request, response) => {
