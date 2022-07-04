8b05fa304d11c70dd2c9a54467072b75ec85fea5
Daniel Byrne <dbyrne978@gmail.com>
2022-07-04 18:15:21 -0400

    Move server connection from mediaObj model to app

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const infoPageRouter = require('./controllers/infoPage')
 const mediaObjArrRouter = require('./controllers/mediaObjArr')
 const userDataRouter = require('./controllers/userData')
 const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const mongoose = require('mongoose')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
 
 // middleware
 app.use(express.json())
--- models/mediaObj.js
+++ models/mediaObj.js
@@ -1,16 +1,4 @@
 const mongoose = require('mongoose')
-const logger = require('../utils/logger')
-const config = require('../utils/config')
-
-logger.info('connecting to', config.MONGODB_URI)
-
-mongoose.connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message)
-  })
 
 const mediaObjSchema = new mongoose.Schema({
   dateCreated: Date,
