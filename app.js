0565b1abc73e08b1843d5047de2e0ce5155ca26b
Daniel Byrne <dbyrne978@gmail.com>
2022-07-04 15:07:51 -0400

    Refactor custom middleware into separate module

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
-const morgan = require('morgan')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const logger = require('./utils/logger')
 const infoPageRouter = require('./controllers/infoPage')
 const mediaObjArrRouter = require('./controllers/mediaObjArr')
 const userDataRouter = require('./controllers/userData')
+const middleware = require('./utils/middleware')
 
 // middleware
 app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
-
-morgan.token('req-body', (req) => JSON.stringify(req.body))
-app.use(morgan('[:date[clf]] :method :url :status :res[content-length] - ' +
-    ':response-time ms -- :req-body'))
+app.use(middleware.morgan('[:date[clf]] :method :url :status ' +
+    ':res[content-length] - :response-time ms -- :req-body'))
 
 // routes
 app.use('/info', infoPageRouter)
@@ -22,24 +19,7 @@ app.use('/api/mediaObjArr', mediaObjArrRouter)
 app.use('/api/userData', userDataRouter)
 
 // more middleware
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(unknownEndpoint)
-
-const errorHandler = (error, request, response, next) => {
-  logger.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
-
-app.use(errorHandler)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
