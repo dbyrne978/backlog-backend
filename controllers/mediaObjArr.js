24b0363934fbcdb3587899ac248956de85a703a6
Daniel Byrne <dbyrne978@gmail.com>
2022-07-03 20:35:58 -0400

    Refactor controllers into seperate module

---
+++ controllers/mediaObjArr.js
@@ -0,0 +1,78 @@
+const mediaObjArrRouter = require('express').Router()
+const MediaObj = require('../models/mediaObj')
+
+mediaObjArrRouter.get('/', (request, response) => {
+  MediaObj.find({}).then(mediaObjArr => {
+    response.json(mediaObjArr)
+  })
+})
+
+mediaObjArrRouter.get('/:id', (request, response, next) => {
+  MediaObj.findById(request.params.id)
+    .then(mediaObj => {
+      if (mediaObj) {
+        response.json(mediaObj)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+mediaObjArrRouter.delete('/:id', (request, response, next) => {
+  MediaObj.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+mediaObjArrRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  //save new mediaObj if title/medium combo doesn't already exist
+  MediaObj
+    .findOne({ title: body.title, medium: body.medium })
+    .then((existingTitleMediumCombo) => {
+      if (existingTitleMediumCombo) {
+        return response.status(400).json({
+          error: 'title/medium combo already exists'
+        })
+      } else {
+        const mediaObj = new MediaObj({
+          dateCreated: new Date(),
+          title: body.title,
+          medium: body.medium,
+          progress: body.progress || false
+        })
+
+        mediaObj.save()
+          .then(savedMediaObj => {
+            response.json(savedMediaObj)
+          })
+          .catch(error => next(error))
+      }
+    })
+})
+
+mediaObjArrRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+
+  const mediaObj = {
+    title: body.title,
+    medium: body.medium,
+    progress: body.progress
+  }
+
+  MediaObj.findByIdAndUpdate(
+    request.params.id,
+    mediaObj,
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedMediaObj => {
+      response.json(updatedMediaObj)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = mediaObjArrRouter
--- models/mediaObj.js
+++ models/mediaObj.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 const logger = require('../utils/logger')
-const config = require('./utils/config')
+const config = require('../utils/config')
 
-logger.info('connecting to', config.url)
+logger.info('connecting to', config.MONGODB_URI)
 
-mongoose.connect(config.url)
+mongoose.connect(config.MONGODB_URI)
   .then(() => {
     logger.info('connected to MongoDB')
   })
